518f064748b899ce45d2854cc8ff65815e76927c
Islem Maboud <islempenywis@gmail.com>
2023-02-20 14:51:20 +0100

    ChatGPT Built Website

--- pages/index.js
+++ pages/index.js
@@ -3,9 +3,10 @@ import Image from "next/image";
 import { Inter } from "@next/font/google";
 import styles from "@/styles/Home.module.css";
 import Navbar from "../components/Navbar";
-import LandingSection from "../components/landingSection";
+import { LandingSection } from "../components/landing";
 import Products from "../components/products";
 import { Footer } from "../components/footer";
+import { Features } from "../components/features";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,8 +20,9 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Navbar />
-      {/* <LandingSection /> */}
+      <LandingSection />
       <Products />
+      <Features />
       <Footer />
     </>
   );
